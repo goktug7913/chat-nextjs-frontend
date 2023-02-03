58fbbe9407cb16d8f64ac875036430e862f488fc
G. Y <goktug7913@gmail.com>
2023-02-03 14:18:13 +0300

    PWA setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <meta
+              name="viewport"
+              content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta charSet={"utf-8"} />
+          <title>Chatter</title>
+          <meta name="description" content="Instant community messaging." />
+          <meta name="keywords" content="chat, messaging, community, instant, free" />
+          <meta name="author" content="Chatter" />
+          <meta name="theme-color" content="#ffffff" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
