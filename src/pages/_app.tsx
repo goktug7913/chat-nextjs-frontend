7fb953e7cc009e45c53f2a2505272be93577a252
goktug7913 <goktug7913@gmail.com>
2023-02-03 12:34:53 +0300

    chat scaffolds, sockets, chat layout, message types and sending

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,11 @@ import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 import AppBar from "@/components/AppBar";
 import {UserProvider} from "@/context/userContext";
-import {SocketProvider, useSocket} from "@/context/socketContext";
+import {SocketProvider} from "@/context/socketContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <div className="flex flex-col bg-indigo-900 min-h-screen">
+      <div className="bg-indigo-900 h-screen flex flex-col">
         <UserProvider>
             <SocketProvider>
                 <AppBar />
--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,31 +1,41 @@
 import MessageItem from "@/components/MessageItem";
 import ChatInputMenu from "@/components/ChatInputMenu";
 import {useSocket} from "@/context/socketContext";
-import {useEffect, useState} from "react";
+import {ReactElement, useEffect, useRef, useState} from "react";
 import Message from "@/types/Message";
 
 export default function Chat() {
     const [messages, setMessages] = useState([] as Message[]);
     const socket = useSocket();
+    const inputMenuRef = useRef<HTMLDivElement>(null);
 
     const InputMenuCallback = (data: Message) => {
         setMessages([...messages, data]);
     }
 
+    // Scroll to bottom on page load, because on mobile, the URL bar is visible and the page is scrolled up
+    const scrollToBottom = () => {
+        if (inputMenuRef.current) {
+            // URL bar is visible, scroll down
+            inputMenuRef.current.scrollIntoView({behavior: "smooth"});
+        }
+    }
+    useEffect(scrollToBottom, []);
+
     useEffect(() => {
         socket.on("msg_tx", (data: Message) => {
-            setMessages([...messages, data]);
+            setMessages([...messages, {...data, isSelf: false}]);
         });
     }, [messages]);
 
     return (
-        <div className="flex flex-col grow">
+        <div className="flex flex-col grow" id="chat-wrapper">
             <div className="flex flex-col overflow-y-auto grow">
                 {messages.map((message) => (
                     <MessageItem key={message.date} message={message} />
                 ))}
             </div>
-            <div className="flex flex-row justify-between w-full h-12 self-end">
+            <div className="justify-between w-full h-12 self-end" ref={inputMenuRef}>
                 <ChatInputMenu setter={InputMenuCallback} messageList={messages}/>
             </div>
         </div>
