f1f7a160b7ba13dc62b9cf381ab004a6d52ff33d
G. Y <goktug7913@gmail.com>
2023-02-03 14:35:34 +0300

    PWA setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,21 @@ import { Analytics } from '@vercel/analytics/react';
 import AppBar from "@/components/AppBar";
 import {UserProvider} from "@/context/userContext";
 import {SocketProvider} from "@/context/socketContext";
+import {useEffect} from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+    useEffect(() => {
+        // Load service worker
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('/sw.js');
+            });
+        } else {
+            console.log("Service worker not supported");
+        }
+    }, []);
+
   return (
       <UserProvider>
           <SocketProvider>
